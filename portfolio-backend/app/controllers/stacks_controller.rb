ccb601e5907be576eaddc149c973f9d2e8aec7f3
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-06-27 15:55:16 -0400

    add namespace api::v1 to set up api

--- portfolio-backend/app/controllers/stacks_controller.rb
+++
@@ -1,2 +0,0 @@
-class StacksController < ApplicationController
-end
--- portfolio-backend/config/routes.rb
+++ portfolio-backend/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :projects
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  // add in api before every route
+  namespace :api do
+    namespace :v1 do
+      resources :comments
+      resources :projects
+      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    end
+  end
 end
+
+# http://localhost:3000/api/v1/projects
