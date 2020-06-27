e80cadc8089899704737e3998a493c96dac23c94
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-06-27 13:43:29 -0400

    add properties to Project table

--- portfolio-backend/app/models/project_stack.rb
+++ portfolio-backend/app/models/project_stack.rb
@@ -1,2 +1,4 @@
 class ProjectStack < ApplicationRecord
+  belongs_to :project
+  belongs_to :stack
 end
--- portfolio-backend/db/migrate/20200627170914_create_projects.rb
+++ portfolio-backend/db/migrate/20200627170914_create_projects.rb
@@ -3,8 +3,10 @@ class CreateProjects < ActiveRecord::Migration[6.0]
     create_table :projects do |t|
       t.string :name
       t.text :description
+      t.string :image_url
       t.string :github_url
       t.string :site_url
+      t.string :blog_url
       t.string :demo_vid
 
       t.timestamps
