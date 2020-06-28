745a55a690697ddfe9c02e0cea03f8566a8bcc08
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-06-28 17:28:36 -0400

    ttest fetch requests to API routes

--- portfolio-frontend/src/App.js
+++ portfolio-frontend/src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   componentDidMount() {
     // we won't do anything with this data until we get resp back
     // take resp and turn into JS object notation
-    fetch('http://localhost:3000/api/v1/projects')
+    fetch('http://localhost:3000/api/v1/projects/')
       // then = we won't do next action until previous resp is finished
       .then(resp => resp.json())
       .then(data => console.log(data))
