754439212c77602ffb52633b3327d70485de2fc9
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-06-29 01:59:28 -0400

    delete fetch  test method in App

--- portfolio-frontend/src/App.js
+++ portfolio-frontend/src/App.js
@@ -4,14 +4,6 @@ import './App.css';
 import ProjectsContainer from './containers/ProjectsContainer'
 
 class App extends Component {
-  componentDidMount() {
-    // we won't do anything with this data until we get resp back
-    // take resp and turn into JS object notation
-    fetch('http://localhost:3000/api/v1/projects/')
-      // then = we won't do next action until previous resp is finished
-      .then(resp => resp.json())
-      .then(data => console.log(data))
-  }
   render() {
     return (
       <div className="App" >
--- portfolio-frontend/src/actions/fetchProjects.js
+++ portfolio-frontend/src/actions/fetchProjects.js
@@ -8,6 +8,8 @@ export const fetchProjects = () => {
     dispatch({ type: 'LOADING_PROJECTS' });
     // call fetch, dispatching returned data
     fetch('http://localhost:3000/api/v1/projects')
+      // we won't do anything with this data until we get resp back
+      // take resp and turn into JS object notation
       // .then() fn to parse JSON of response to this request
       .then(resp => resp.json())
       // execute asynchronous code, once resolved, we can use dispatch to update our store
