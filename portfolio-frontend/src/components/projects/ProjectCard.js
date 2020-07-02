71564058094e98819d22bd683ef310b8a32f2435
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-07-02 12:37:46 -0400

    display project stacks on each projectCard

--- portfolio-frontend/src/components/projects/ProjectCard.js
+++ portfolio-frontend/src/components/projects/ProjectCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const ProjectCard = ({ project }) => {
+  const projectStacks = project.stacks.map(stack => stack.name).join(' | ');
+
   return (
     <div>
       <h2>{project.name}</h2>
       <img className='project-thumbnail' alt='project thumbnail' src={project.image_url} />
-      <i>{project.stacks.join(' | ')}</i>
+      <i>{projectStacks}</i>
       <hr />
       {/* <iframe src="https://drive.google.com/file/d/1WwtgDcBwrXa-tJ1pXbyBwBBkClDNdvst/preview" width="640" height="480"></iframe> */}
       {/* <CommentsContainer comments={project.comments} /> */}
