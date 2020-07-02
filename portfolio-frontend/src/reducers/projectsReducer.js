bd00c9c46e045ab4c7d07d275c0cec251da9ce46
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-07-02 11:54:56 -0400

    aadd LOADING_STACKS and ADD_STACKS case statements to projectsReducer

--- portfolio-frontend/src/reducers/projectsReducer.js
+++ portfolio-frontend/src/reducers/projectsReducer.js
@@ -1,8 +1,22 @@
 // when action is dispatched to reducer,
 // reducer is automatically passed previous state as 1st arg
 // Everytime we update sth in our store, this reducer will be returning the updated state
-const projectsReducer = (state = { projects: [], comments: [], loading: false }, action) => {
+const projectsReducer = (state = { projects: [], stacks: [], comments: [], loading: false }, action) => {
   switch (action.type) {
+    case 'LOADING_STACKS':
+      return {
+        ...state,
+        stacks: [...state.stacks],
+        loading: true
+      }
+
+    case 'ADD_STACKS':
+      return {
+        ...state,
+        stacks: action.stacks,
+        loading: false
+      }
+
     case 'LOADING_PROJECTS':
       return {
         ...state,
