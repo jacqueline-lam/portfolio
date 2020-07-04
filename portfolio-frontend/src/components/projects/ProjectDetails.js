d4c9ba60d0c19b651fb173b079d458083a41f068
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-07-04 14:13:21 -0400

    delete idle code in ProjectDetails

--- portfolio-frontend/src/components/projects/ProjectDetails.js
+++ portfolio-frontend/src/components/projects/ProjectDetails.js
@@ -1,29 +1,5 @@
 import React from 'react';
 
-// const ProjectDetails = ({ match, projects }) => {
-// const renderProject = project => {
-// if (!!project) {
-//   const projectStacks = project.stacks.map(stack => stack.name).join(' | ');
-//   return (
-//     <div>
-//       <h1>{project.name}</h1>
-//       <img className='project-thumbnail' alt='project thumbnail' src={project.image_url} />
-//       <i>{projectStacks}</i>
-//       <p>{project.description}</p>
-
-//       <button onClick={() => window.open(project.github_url, "_blank")}>Source Code</button>
-//       <button onClick={() => window.open(project.blog_url, "_blank")}>Blog Post</button>
-//       {/* <h4>Project Demo</h4> */}
-//       {/* {project.demo_vid} */}
-//       {/* <CommentsContainer comments={project.comments} /> */}
-//     </div>
-//   )
-// } else {
-//   return (<h3>Error 404: Project does not exist.</h3>)
-// }
-// }
-
-
 const ProjectDetails = ({ match, projects }) => {
   console.log(match)
   console.log(projects)
@@ -37,9 +13,10 @@ const ProjectDetails = ({ match, projects }) => {
         <img className='project-thumbnail' alt='project thumbnail' src={project.image_url} />
         <i>{projectStacks}</i>
         <p>{project.description}</p>
-
         <button className="btn btn-outline-warning" onClick={() => window.open(project.github_url, "_blank")}>Source Code</button>
-        <button className="btn btn-outline-warning" onClick={() => window.open(project.blog_url, "_blank")}>Blog Post</button>
+        <button className="btn btn-outline-warning" onClick={() => window.open(project.blog_url, "_blank")}>Blog Post</button><br /><br />
+        <h5>Project Demo</h5>
+        {/* {project.demo_vid} */}
         <hr />
       </div>
     ]
@@ -49,48 +26,10 @@ const ProjectDetails = ({ match, projects }) => {
   return (
     < >
       {project ? renderProject() : null}
-      {/* <h3>{project.name}</h3>
-      <img className='project-thumbnail' alt='project thumbnail' src={project.image_url} />
-      <i>{projectStacks}</i>
-      <p>{project.description}</p>
 
-      <button className="btn btn-outline-warning" onClick={() => window.open(project.github_url, "_blank")}>Source Code</button>
-      <button className="btn btn-outline-warning" onClick={() => window.open(project.blog_url, "_blank")}>Blog Post</button> */}
-      {/* <h4>Project Demo</h4>
-      {/* {project.demo_vid} */}
       {/* <CommentsContainer comments={project.comments} /> */}
     </>
   )
 }
-// console.log(match.params.projectId)
-// console.log(projects)
-
-// let project = projects.find(proj => proj.id.toString() === match.params.projectId)
-// const renderProject = () => {
-//   if (!!project) {
-//     const projectStacks = project.stacks.map(stack => stack.name).join(' | ');
-//     return (
-//       <div>
-//         <h1>{project.name}</h1>
-//         <img className='project-thumbnail' alt='project thumbnail' src={project.image_url} />
-//         <i>{projectStacks}</i>
-//         <p>{project.description}</p>
-
-//         <button onClick={() => window.open(project.github_url, "_blank")}>Source Code</button>
-//         <button onClick={() => window.open(project.blog_url, "_blank")}>Blog Post</button>
-//         {/* <h4>Project Demo</h4> */}
-//         {/* {project.demo_vid} */}
-//         {/* <CommentsContainer comments={project.comments} /> */}
-//       </div>
-//     )
-//   } else {
-//     return (<h3>Error 404: Project does not exist.</h3>)
-//   }
-// }
-// return (
-//   // renderProject()
-//   <div>Test</div>
-// )
-// }
 
 export default ProjectDetails
