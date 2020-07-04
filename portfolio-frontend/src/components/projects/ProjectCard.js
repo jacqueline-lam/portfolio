07bb84bb65e3ddfe63b41a651bd26fdc7b9ebb9a
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-07-04 15:31:03 -0400

    remove unused component imports

--- portfolio-frontend/src/components/projects/ProjectCard.js
+++ portfolio-frontend/src/components/projects/ProjectCard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ProjectDetails from './ProjectDetails'
 import { Link } from 'react-router-dom';
 
 const ProjectCard = ({ project }) => {
--- portfolio-frontend/src/components/projects/ProjectList.js
+++ portfolio-frontend/src/components/projects/ProjectList.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ProjectCard from './ProjectCard'
-import ProjectDetails from './ProjectDetails'
 // import CommentsContainer from '../../containers/CommentsContainer'
-// import Project from './Project'
 
 const ProjectList = ({ filteredProjects }) => {
   const renderProjects = filteredProjects.map(project =>
