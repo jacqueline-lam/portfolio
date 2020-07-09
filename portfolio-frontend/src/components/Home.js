e713b4748c2caa12ccb878d1f1e4874721521af2
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-07-08 21:18:21 -0400

    add div tag to home component and add 'read post' button in blogPost component to link to external blog post

--- portfolio-frontend/src/components/Home.js
+++ portfolio-frontend/src/components/Home.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <h1>Hello, I'm Jacqueline. I'm a Full Stack Software Developer.</h1>
-    <i>When I'm not coding, you'll find me climbing at the local bouldering gym or experimenting with new recipes like miso basil pasta!</i>
+    <div>
+      <h1>Hello, I'm Jacqueline. I'm a Full Stack Software Developer.</h1>
+      <i>When I'm not coding, you'll find me climbing at the local bouldering gym or experimenting with new recipes like miso basil pasta!</i>
+    </div>
   )
 }
 
--- portfolio-frontend/src/components/blog/BlogPost.js
+++ portfolio-frontend/src/components/blog/BlogPost.js
@@ -4,10 +4,11 @@ import Moment from 'react-moment';
 const BlogPost = ({ blogPost }) => {
   return (
     <div>
-      <img className='project-thumbnail' alt='project thumbnail' src={blogPost.cover_image} />
+      <img className='blog-post-thumbnail' alt='blog post thumbnail' src={blogPost.cover_image} />
       <h6>{blogPost.title}</h6>
       <i><Moment format='MMM DD, YYYY'>{blogPost.published_timestamp}</Moment></i>
       <p>{blogPost.description}</p>
+      <button className="btn btn-danger btn-lg" onClick={() => window.open(blogPost.url, "_blank")}>READ POST</button>
     </div >
   );
 };
