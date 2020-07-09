7a6c3c8a926351ade35cfb4e5e479de6db083968
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-07-08 21:28:24 -0400

    refactor blogPost component to display blog posts in react-bootsrap card format

--- portfolio-frontend/src/components/blog/BlogPost.js
+++ portfolio-frontend/src/components/blog/BlogPost.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Moment from 'react-moment';
+import Card from 'react-bootstrap/Card';
 
 const BlogPost = ({ blogPost }) => {
   return (
-    <div>
-      <img className='blog-post-thumbnail' alt='blog post thumbnail' src={blogPost.cover_image} />
+    <div className='blog'>
+      <Card style={{ width: '60%' }}>
+        <Card.Body>
+          <Card.Img variant='top' src={blogPost.cover_image} ></Card.Img>
+          <Card.Title>{blogPost.title}</Card.Title>
+          <Card.Text>{blogPost.description}</Card.Text>
+          <button className="btn btn-danger btn-lg" onClick={() => window.open(blogPost.url, "_blank")}>READ POST</button>
+        </Card.Body>
+        <Card.Footer>
+          <small className='text-muted'><Moment format='MMM DD, YYYY'>{blogPost.published_timestamp}</Moment></small>
+        </Card.Footer>
+      </Card>
+      {/* <img className='blog-post-thumbnail' alt='blog post thumbnail' src={blogPost.cover_image} />
       <h6>{blogPost.title}</h6>
       <i><Moment format='MMM DD, YYYY'>{blogPost.published_timestamp}</Moment></i>
-      <p>{blogPost.description}</p>
-      <button className="btn btn-danger btn-lg" onClick={() => window.open(blogPost.url, "_blank")}>READ POST</button>
+      <p>{blogPost.description}</p> */}
+      {/* <button className="btn btn-danger btn-lg" onClick={() => window.open(blogPost.url, "_blank")}>READ POST</button> */}
     </div >
   );
 };
