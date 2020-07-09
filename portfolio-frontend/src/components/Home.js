0a8dc053a8af372c285261b1a3c95c15e517fa84
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-07-08 20:41:22 -0400

    edit homepage welcome message

--- portfolio-frontend/src/components/Home.js
+++ portfolio-frontend/src/components/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <h1>Jacqueline Lam</h1>
+    <h1>Hello, I'm Jacqueline. I'm a Full Stack Software Developer.</h1>
+    <i>When I'm not coding, you'll find me climbing at the local bouldering gym or experimenting with new recipes like miso basil pasta!</i>
   )
 }
 
