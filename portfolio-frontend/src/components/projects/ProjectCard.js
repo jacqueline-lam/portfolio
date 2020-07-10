901e4682800051e8a21fdb5e9283cb8f8ffc0adb
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-07-09 21:04:45 -0400

    change the imgae_url of each project to local image file name and dynamically render each project's image in ProjectDetails and ProjectList components

--- portfolio-frontend/src/components/projects/ProjectCard.js
+++ portfolio-frontend/src/components/projects/ProjectCard.js
@@ -4,13 +4,18 @@ import { Link } from 'react-router-dom';
 const ProjectCard = ({ project }) => {
   const projectStacks = project.stacks.map(stack => stack.name).join(' | ');
 
+  const fileName = project.image_url
+  // special feature supported by webpack's compiler - allows you to get all matching modules starting from some base directory
+  const images = require.context('../../images', true)
+  let img = images('./' + fileName)
+
   return (
     <div>
       <Link key={project.id} to={`/projects/${project.id}`}>
-        {project.name}
+        <h4>{project.name}</h4>
       </Link>
       {/* <h3><a href="#">{project.name}</a></h3> */}
-      <img className='project-thumbnail' alt='project thumbnail' src={project.image_url} />
+      <img className='project-thumbnail' alt='project thumbnail' src={img} />
       <i>{projectStacks}</i>
       <hr />
       {/* <iframe src="https://drive.google.com/file/d/1WwtgDcBwrXa-tJ1pXbyBwBBkClDNdvst/preview" width="640" height="480"></iframe> */}
--- portfolio-frontend/src/components/projects/ProjectDetails.js
+++ portfolio-frontend/src/components/projects/ProjectDetails.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import CommentsContainer from '../../containers/CommentsContainer'
+// import BoldererAppPic from '../../../images/bolderer-sinatra-app_homepage.png'
+// import projectPic from `../../../images/${project.image_url}`
 
 const ProjectDetails = ({ match, projects }) => {
 
@@ -12,11 +14,16 @@ const ProjectDetails = ({ match, projects }) => {
         )}
       </div>
     );
+    const fileName = project.image_url
+    // special feature supported by webpack's compiler that allows you to get all matching modules starting from some base directory
+    const images = require.context('../../images', true)
+    let img = images('./' + fileName)
 
     return [
       <div>
         <h3>{project.name}</h3>
-        <img className='project-thumbnail' alt='project thumbnail' src={project.image_url} />
+        {/* <img className='project-thumbnail' alt='project thumbnail' src={projectPic} /> */}
+        {<img className='project-thumbnail' alt='project thumbnail' src={img} />}
         <h5>Stacks</h5>
         {projectStacks}
         <p>{project.description}</p>
@@ -27,12 +34,11 @@ const ProjectDetails = ({ match, projects }) => {
           <iframe title="Umami Pantry Demo" width="560" height="315" src={project.demo_vid} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
         </div>
         <hr />
-        <img alt="test" src="../../../../portfolio-backend/app/assets/images/rails-bolderer-app_all-jackies-sends" />
+
         <CommentsContainer project={project} />
       </div>
     ]
   }
-
 
   return (
     < >
@@ -40,5 +46,4 @@ const ProjectDetails = ({ match, projects }) => {
     </>
   )
 }
-
 export default ProjectDetails
