f9e4497a2b9b76e56c3ec44b5a434b618ae548c3
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-07-19 00:01:12 -0400

    add initial state in store to rootReducer

--- portfolio-frontend/src/reducers/rootReducer.js
+++ portfolio-frontend/src/reducers/rootReducer.js
@@ -2,6 +2,23 @@ import { combineReducers } from "redux";
 import projectsReducer from './projectsReducer'
 import blogPostsReducer from './blogPostsReducer'
 
+// initial state:
+// state = {
+//   projects: {
+//     allProjects: [],
+//     stacks: [],
+//     selectedStackIds: [],
+//     filteredProjects: [],
+//     loading: false,
+//   },
+//   blog: {
+//     blogPosts: [],
+//     loadingBlog: false
+//   }
+// }
+
+
+// delegate diff pieces of state to separate reducer functions
 const rootReducer = combineReducers({
   projects: projectsReducer,
   blog: blogPostsReducer
