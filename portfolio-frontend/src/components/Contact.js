b1978a173d5f8d33cfb83b5d0e87334934b30683
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-07-23 12:44:13 -0400

    remove contact form code from Contact component

--- portfolio-frontend/src/components/Contact.js
+++ portfolio-frontend/src/components/Contact.js
@@ -8,23 +8,6 @@ const Contact = () => {
   const emailImg = images('./email.png')
 
   return (
-    // <div id='contact' className='component'>
-    //   <h1>CONTACT ME</h1>
-    //   <h2>Connect on Social</h2>
-    //   <div className='icons'>
-    //     <a alt="linkedin_icon" href="https://www.linkedin.com/in/jacquelineklam/" target="_blank">
-    //       <img src={linkedinImg} />
-    //     </a>
-    //     <a alt="github_icon" href="https://github.com/jacqueline-lam" target="_blank">
-    //       <img src={githubImg} />
-    //     </a>
-    //     <a alt="blog_icon" href="https://dev.to/jacquelinelam" target="_blank">
-    //       <img src={blogImg} />
-    //     </a>
-    //   </div>
-    //   <div>Send me a message</div>
-    // </div>
-
     <div id='contact' className='sidenav'>
       <div className='decoration-line'>CONTACT</div>
       <a href="https://www.linkedin.com/in/jacquelineklam/" target="_blank" rel="noopener noreferrer">
