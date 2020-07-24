737aa0e95a0b8a773c33a7fa83ef3fc8d335a376
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-07-24 03:11:50 -0400

    change navbar -brand href to homepage

--- portfolio-frontend/src/components/NavBar.js
+++ portfolio-frontend/src/components/NavBar.js
@@ -1,14 +1,12 @@
 import React from 'react';
-// import { LinkContainer } from 'react-router-bootstrap'
 import { NavLink } from 'react-router-dom';
-// import { Navbar, Form, FormControl, Button, Nav } from 'react-bootstrap';
 // NavLink = superset of Link, adding styling attrs to rendered ele
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
       <div className="container">
-        <a className="navbar-brand" href="#page-top">Jacqueline Lam</a>
+        <a className="navbar-brand" href="/">Jacqueline Lam</a>
         <div className="justify-content-end" id="navbarDefault" >
           <ul className="navbar-nav">
             <li className='nav-item'>
@@ -51,16 +49,6 @@ const NavBar = () => {
                 }}
               >Blog</NavLink>
             </li>
-            {/* <li className='nav-item'>
-              <NavLink
-                className='nav-link'
-                to="/contact"
-                exact
-                activeStyle={{
-                  color: '#bda38f'
-                }}
-              >Contact</NavLink>
-            </li> */}
           </ul>
         </div>
       </div>
