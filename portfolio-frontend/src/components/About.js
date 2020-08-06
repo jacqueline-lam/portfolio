83560f1813f3a1487342e446eb76e5be74d1541c
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-08-06 10:57:56 -0400

    edit hobby description on about page to include personality traits

--- portfolio-frontend/src/components/About.js
+++ portfolio-frontend/src/components/About.js
@@ -5,11 +5,11 @@ const About = () => {
     <div id='about' className='component'>
       <div className='text' id='about-text'>
         <h1>ABOUT ME</h1>
-        <h2>Digital Marketer Turned Software Engineer</h2>
+        <h2>Full-stack Software Engineer</h2>
         <div className='text-section'>
           <h6>My passion for using technologies to cultivate brands and develop strategies have led me to a path of software engineering.</h6>
           <h6>I enjoy experimenting with code to tackle both simple and complex problems.</h6>
-          <i>In my spare time, you will find me climbing at the local bouldering gym or experimenting with new recipes like miso basil pasta!</i>
+          <i>The rock climber in me gives me the flexibility and adaptability to be a multi-faceted problem solver under intense pressure, and imbues a passion for consistent self-improvement.</i>
         </div>
         <div className='text-section' id='skills'>
           <h4> // Technical Skills</h4>
