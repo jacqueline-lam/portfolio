8fd3e73681fc634951a098866536c331bfd725da
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-08-14 23:59:25 -0400

    replace default logo.svg with new logo

--- portfolio-frontend/src/App.js
+++ portfolio-frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import { Route } from 'react-router-dom';
 import 'bootswatch/dist/litera/bootstrap.min.css';
 import './App.css';
